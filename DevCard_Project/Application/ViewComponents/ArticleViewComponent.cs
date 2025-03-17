6b262f2b73f6127cff0659763c7e0f416d06abc5
98913 <98913@LAPTOP-JQR66TAF>
2025-03-17 11:50:48 +0330

    Edit Sections

--- DevCard_Project/Application/ViewComponents/ArticleViewComponent.cs
+++ DevCard_Project/Application/ViewComponents/ArticleViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.ViewComponents;
 
@@ -7,6 +8,13 @@ public class ArticleViewComponent:ViewComponent
 
     public IViewComponentResult Invoke()
     {
-        return View("_Article");
+        return View("_Article",new List<ArticleModel>
+        {
+            new ArticleModel{Id = new Guid(),Name = "Docker Essentials",Url = "https://www.linkedin.com/pulse/docker-essentials-mohammad-javad-hejazi-g7mif/?trackingId=E336Avk7oF43yrCv0JOYnA%3D%3D",Description = "Docker: An open-source platform that automates the deployment, scaling, and management of applications in lightweight containers. It allows developers to package applications with all dependencies into a standardized unit,...",Image = "Docker.png"},
+            new ArticleModel{Id = new Guid(),Name = " Railway Oriented Programming",Url = "https://www.linkedin.com/posts/mohammad-javad-hejazi-749302247_programming-functionalprogramming-errorhandling-activity-7185525169694662657-TYml?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD0XZakBg9mOnWVP6oqJWnw1n7pJwd2KU6Q",Description = "Have you heard of Railway Oriented Programming (ROP)?",Image = "Railway Oriented Programming.png"},
+            new ArticleModel{Id = new Guid(),Name = " CLR",Url = "https://www.linkedin.com/posts/mohammad-javad-hejazi-749302247_dotnet-csharp-clr-activity-7179719600740155392-BRaW?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD0XZakBg9mOnWVP6oqJWnw1n7pJwd2KU6Q",Description = "Excited to share insights on the core components of the .NET framework",Image = "CLR.png"},
+
+            
+        });
     }
 }
--- DevCard_Project/Application/ViewComponents/ProjectViewComponent.cs
+++ DevCard_Project/Application/ViewComponents/ProjectViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Application.Models;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Application.ViewComponents;
@@ -6,8 +7,14 @@ namespace Application.ViewComponents;
 public class ProjectViewComponent:ViewComponent
 {
     public IViewComponentResult Invoke()
-    {
-        return View("_Project");
+    {   
+        return View("_Project",new List<ProjectModel>
+        {
+            new ProjectModel{Id = new Guid(),Name = "DevCard",Url = "https://github.com/hejazij88/DevCard",Description = "The Portfolio And Don't Use DataBase , Use View Component And Static Data",Image = "PortFolio.jpg"},
+            new ProjectModel{Id = new Guid(),Name = "BlazorChatRoom",Url = "https://github.com/hejazij88/BlazorChatRoom",Description = "The Chat Room By Blazor ",Image = "ChatRoom.png"},
+            new ProjectModel{Id = new Guid(),Name = "TorontoShop",Url = "https://github.com/hejazij88/TorontoShop",Description = "The Online Shop by Asp.net Core ",Image = "OnlineShop.jpg"},
+            new ProjectModel{Id = new Guid(),Name = "Virgol",Url = "https://github.com/hejazij88/Virgol",Description = "The Online Article Web Site by Asp.net MVC ",Image = "Virgol.jpg"},
+        });
     }
     
 }
